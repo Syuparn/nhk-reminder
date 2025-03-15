3622fe4057126c805e7f01fde2b399162dd7630c
syuparn <s.hello.spagetti@gmail.com>
2025-03-15 09:39:06 +0900

    init clasp project

---
+++ main.ts
@@ -0,0 +1,14 @@
+/*
+type Calender = {
+  title: string
+  startDate: Date
+  endDate: Date
+  options: {
+    description: string
+  }
+}
+*/
+
+function handler() {
+  console.log("Hello, world!");
+}
